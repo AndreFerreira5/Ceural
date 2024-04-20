a660348d1e4bbaf84bcbb7681d31c08cca0b6caf
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2024-04-20 17:44:54 +0100

    activation functions

--- src/activations.c
+++ src/activations.c
@@ -1,1 +1,36 @@
 #include "activations.h"
+#include "utils.h"
+
+
+double sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+}
+
+
+double sigmoid_derivative(double x) {
+    double s = sigmoid(x);
+    return s * (1 - s);
+}
+
+
+double relu(double x) {
+    return x > 0 ? x : 0;
+}
+
+
+double relu_derivative(double x) {
+    return x > 0 ? 1 : 0;
+}
+
+
+double tanh(double x){
+    double epx = exp(x);
+    double enx = exp(-x);
+    return (epx-enx)/(epx+enx);
+}
+
+
+double tanh_derivative(double x){
+    double tanh_value = tanh(x);
+    return 1 - (tanh_value*tanh_value);
+}
--- src/activations.h
+++ src/activations.h
@@ -1,4 +1,9 @@
 #ifndef DIGITS_NN_C_ACTIVATIONS_H
 #define DIGITS_NN_C_ACTIVATIONS_H
 
+double sigmoid(double x);
+double sigmoid_derivative(double x);
+double relu(double x);
+double relu_derivative(double x);
+
 #endif //DIGITS_NN_C_ACTIVATIONS_H
