e664fec7a566f4ca0c0b149fb8f28e9329185ae6
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2024-04-20 22:26:07 +0100

    loss functions

--- src/loss.c
+++ src/loss.c
@@ -1,1 +1,26 @@
 #include "loss.h"
+
+double mean_squared_error_loss(const size_t output_size, const double *network_outputs, const double *expected_output){
+    double sum = 0;
+    for(size_t output_neuron=0; output_neuron<output_size; ++output_neuron){
+        sum += pow((network_outputs[output_neuron] - expected_output[output_neuron]), 2);
+    }
+    return (1/(double)output_size)*sum;
+}
+
+
+double multi_class_cross_entropy_loss(const size_t output_size, const double *network_outputs, const double *expected_output){
+    double sum = 0;
+    for(size_t output_neuron=0; output_neuron<output_size; ++output_neuron){
+        sum += expected_output[output_neuron] * log(network_outputs[output_neuron]);
+    }
+    return -sum;
+}
+
+
+double binary_cross_entropy_loss(const size_t output_size, const double *network_outputs, const double *expected_output){
+    double correct_class = -1;
+    if(expected_output[0] == 1) correct_class = 0;
+    if(expected_output[1] == 1) correct_class = 1;
+    return -(correct_class*log(network_outputs[1]) + (1-correct_class)*log(1-network_outputs[1]));
+}
--- src/loss.h
+++ src/loss.h
@@ -1,4 +1,10 @@
 #ifndef DIGITS_NN_C_LOSS_H
 #define DIGITS_NN_C_LOSS_H
 
+#include "utils.h"
+
+double mean_squared_error_loss(size_t output_size, const double *network_outputs, const double *expected_output);
+double multi_class_cross_entropy_loss(size_t output_size, const double *network_outputs, const double *expected_output);
+double binary_cross_entropy_loss(size_t output_size, const double *network_outputs, const double *expected_output);
+
 #endif //DIGITS_NN_C_LOSS_H
